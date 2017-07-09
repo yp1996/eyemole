41d409375e1c9a8193c683e8ecaa6a4132078c64
terangg <twesa96@gmail.com>
2017-07-08 17:12:22 -0700

    meh

---
+++ BCI_Unity/Assets/RotateLEftRight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLEftRight : MonoBehaviour {
+
+	private double frontAsymmetry;
+
+	// Use this for initialization
+	void Start () {
+		frontAsymmetry = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		frontAsymmetry = OSCTest.eegData [3] - OSCTest.eegData [2];
+		transform.Translate((float) frontAsymmetry, 0, 0);
+		
+	}
+}
